bebfe6b319df394ebc278bb4909baf858652884f
Adhiraj Jain <adhirajjain2001@gmail.com>
2022-05-01 14:04:05 +1000

    Added footer with changes

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -4,16 +4,16 @@ import "../../Stylesheets/Footer.css";
 const Footer = () => {
     return (
         <section id="footer">
-            <div class="container">
+            <div className="container">
                 <ul className="d-flex flex-row justify-content-center align-items-center">
                     <li>
-                        <a href="https://www.linkedin.com/in/adhiraj-jain/"><i class="fab fa-linkedin-in"></i></a>
+                        <a href="https://www.linkedin.com/in/adhiraj-jain/"><i className="fab fa-linkedin-in"></i></a>
                     </li>
                     <li>
-                        <a href="https://github.com/Adhiraj-Jain"><i class="fab fa-github"></i></a>
+                        <a href="https://github.com/Adhiraj-Jain"><i className="fab fa-github"></i></a>
                     </li>
                     <li>
-                        <a href="https://twitter.com/adhirajjain09"><i class="fab fa-twitter"></i></a>
+                        <a href="https://twitter.com/adhirajjain09"><i className="fab fa-twitter"></i></a>
                     </li>
 
                 </ul>
