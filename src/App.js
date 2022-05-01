f9fbda87d687ca1d5a5e619d25bdb7a626e36d59
Adhiraj Jain <adhirajjain2001@gmail.com>
2022-05-01 14:05:19 +1000

    Removed react router dom and switch

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
-// import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Layout/Header';
 import About from "./components/About/About";
 import Skills from './components/Skills/Skills';
 import Experience from './components/Experience/Experience';
-
-// <img src={logo} className="App-logo" alt="logo" />
-// <p>
-//   Edit <code>src/App.js</code> and save to reload.
-// </p>
-// <a
-//   className="App-link"
-//   href="https://reactjs.org"
-//   target="_blank"
-//   rel="noopener noreferrer"
-// >
-//   Learn React
-// </a>
+import Projects from './components/Projects/Projects';
+import Quote from './components/Quote/Quote';
+import ContactMe from './components/ContactMe/ContactMe';
+import Footer from './components/Layout/Footer';
+import NavBar from './components/Layout/NavBar';
+import Popup from './components/Projects/Popup';
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [projectDetails, setProjectDetails] = useState(null);
+
+  const openModal = (project) => {
+    console.log(project.value);
+    setProjectDetails(project)
+    setShowModal(prev => !prev);
+  };
   return (
     <div className="App">
-      <Header className="App-header" />
+      <Popup className="modal" trigger={showModal} setShowModal={setShowModal} projectDetails={projectDetails} />
+      <NavBar />
+      <Header />
       <About />
       <Skills />
       <Experience />
+      <Projects openModal={openModal} />
+      <Quote />
+      <ContactMe />
+      <Footer />
     </div>
   );
 }
