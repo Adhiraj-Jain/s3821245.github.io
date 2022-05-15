0d4d327b2e742749b81348d1bdeb67876d1fb22c
Adhiraj Jain <adhirajjain2001@gmail.com>
2022-05-15 20:37:28 +1000

    Fixed footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -3,23 +3,19 @@ import "../../Stylesheets/Footer.css";
 
 const Footer = () => {
     return (
-        <section id="footer">
-            <div className="container">
-                <ul className="d-flex flex-row justify-content-center align-items-center">
-                    <li>
-                        <a href="https://www.linkedin.com/in/adhiraj-jain/"><i className="fab fa-linkedin-in"></i></a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/Adhiraj-Jain"><i className="fab fa-github"></i></a>
-                    </li>
-                    <li>
-                        <a href="https://twitter.com/adhirajjain09"><i className="fab fa-twitter"></i></a>
-                    </li>
-
-                </ul>
-            </div>
-
-        </section>
+        <div id="footer" className='footer'>
+            <ul className="">
+                <li>
+                    <a href="https://www.linkedin.com/in/adhiraj-jain/"><i className="fab fa-linkedin-in"></i></a>
+                </li>
+                <li>
+                    <a href="https://github.com/Adhiraj-Jain"><i className="fab fa-github"></i></a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/adhirajjain09"><i className="fab fa-twitter"></i></a>
+                </li>
+            </ul>
+        </div>
     );
 };
 
