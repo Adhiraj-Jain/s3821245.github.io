97fea0906887d787610b5609085198c534d58c72
Adhiraj Jain <adhirajjain2001@gmail.com>
2022-06-11 22:27:56 +1000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import './Stylesheets/App.css';
 import Header from './components/Layout/Header';
 import About from "./components/About/About";
 import Skills from './components/Skills/Skills';
@@ -10,10 +10,25 @@ import ContactMe from './components/ContactMe/ContactMe';
 import Footer from './components/Layout/Footer';
 import NavBar from './components/Layout/NavBar';
 import Popup from './components/Projects/Popup';
+import ResumeBar from './components/ResumeBar/ResumeBar';
 
-function App() {
+const App = () => {
   const [showModal, setShowModal] = useState(false);
   const [projectDetails, setProjectDetails] = useState(null);
+  const [pageHeight, setPageHeight] = useState(0);
+  const [pageWidth, setPageWidth] = useState(0);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (ref.current !== null) {
+      setPageHeight(ref.current.clientHeight);
+    }
+  }, []);
+  useEffect(() => {
+    if (ref.current !== null) {
+      setPageWidth(window.innerWidth);
+    }
+  }, []);
 
   const openModal = (project) => {
     console.log(project.value);
@@ -21,17 +36,20 @@ function App() {
     setShowModal(prev => !prev);
   };
   return (
-    <div className="App">
+    <div className="App" >
       <Popup className="modal" trigger={showModal} setShowModal={setShowModal} projectDetails={projectDetails} />
-      <NavBar />
-      <Header />
-      <About />
-      <Skills />
-      <Experience />
-      <Projects openModal={openModal} />
-      <Quote />
-      <ContactMe />
-      <Footer />
+      <div ref={ref}>
+        <ResumeBar pageHeight={pageHeight} />
+        <NavBar pageHeight={pageHeight} />
+        <Header pageWidth={pageWidth} pageHeight={pageHeight} />
+        <About />
+        <Skills />
+        <Experience />
+        <Projects openModal={openModal} />
+        <Quote />
+        <ContactMe />
+        <Footer />
+      </div>
     </div>
   );
 }
