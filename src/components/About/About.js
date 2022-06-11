a9159650cad16c6a0a26916f8d4fa98294d1b1bf
Adhiraj Jain <adhirajjain2001@gmail.com>
2022-06-11 22:25:39 +1000

    Fixed about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,13 +5,14 @@ import Education from './Education';
 import $ from 'jquery';
 import Certification from "./Certification";
 import Awards from "./Awards"
+import mydata from '../../resume.json';
 
 const About = () => {
     const [category, setCategory] = useState('1');
     const showContent = (name) => {
-        // if(name == 1)
         setCategory(name.target.value)
     }
+    const aboutJson = mydata['about']
 
     $(".about-li").on("click", function () {
         $(".about-li").removeClass("active");
@@ -28,18 +29,14 @@ const About = () => {
                     <div className="col-lg-10">
                         <span className="section-headings about-text">About me</span>
                         <h3 className="about-heading mt-3">Software Developer based in Australia</h3>
-                        <p className="about-para mt-4">I'm web designer, and I'm very passionate and dedicated to my work.
-                            With 20 years experience as a professional web developer,
-                            I have acquired the skills and knowledge necessary to make your project a success.
-                            I enjoy every step of the design process, from discussion and collaboration.</p>
-                        <ul className="d-flex flex-row justify-content-start about-ul">
-                            {/* <li onClick={showContent} className="about-li active">
-                                <button className="" value="1" >Education & Certification</button>
-                            </li> */}
+                        {aboutJson['aboutme'].map((text, index) => {
+                            return (<p key={text} className="about-para mt-4">{text}</p>)
+                        })}
+
+                        <ul className="about-ul">
                             <AboutMenu showContents={showContent} value='1' name="Education" setActive={true} />
                             <AboutMenu showContents={showContent} value='2' name="Certifications" />
                             <AboutMenu showContents={showContent} value='3' name="Awards" />
-                            {/* <AboutMenu showContents={showContent} value='4' name="Experience" /> */}
                         </ul>
                         {category === '1' && <Education />}
                         {category === '2' && <Certification />}
