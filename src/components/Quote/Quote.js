07662c1f6c79a14bf90c5f76e2d7a894d8a0050e
Adhiraj Jain <adhirajjain2001@gmail.com>
2022-06-11 22:05:34 +1000

    Changed div to section

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -3,12 +3,12 @@ import "../../Stylesheets/Quote.css";
 
 const Quote = () => {
     return (
-        <div id="Quote">
+        <section id="Quote">
             <div className="inside">
                 <span className="quote">“Whenever I am in a difficult situation where there seems to be no way out, I think about all the times I have been in such situations and say to myself: I did it before, so I can do it again.”</span>
                 <span className="author">- Idowu Koyenikan</span>
             </div>
-        </div>
+        </section>
     );
 };
 
