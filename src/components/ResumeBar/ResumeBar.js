4fe74ddf7dfa058b5a2e8db84797cd637d82fb91
Adhiraj Jain <adhirajjain2001@gmail.com>
2022-06-11 22:05:04 +1000

    Implemented ResumeBar

---
+++ src/components/ResumeBar/ResumeBar.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Resume from '../../resume.json';
+import '../../Stylesheets/ResumeBar.scss';
+
+const ResumeBar = () => {
+    const [navBar, setNavBar] = useState(false);
+
+    const changeBackground = () => {
+
+        if (window.scrollY >= 50) {
+            setNavBar(true)
+        }
+        else {
+            setNavBar(false)
+        }
+    }
+
+    window.addEventListener('scroll', changeBackground)
+
+    const navBarClassName = 'resumeContainer' + (navBar ? ' scroll' : '');
+    return (
+        <section className={navBarClassName}>
+            <a className="resume-brand text" href="#">{Resume.about.name}</a>
+            <ul className="resume-ul">
+                <li className="">
+                    <a className="text" onClick={() => window.open("https://drive.google.com/file/d/1J6Ceo0InAXNVI1y1waFTaIvGY62Enjfq/view?usp=sharing", "_blank")} aria-current="page" href="#">Resume</a>
+                </li>
+            </ul>
+        </section>
+    );
+};
+
+export default ResumeBar;
