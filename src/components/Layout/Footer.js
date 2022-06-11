8d98f6107925338ec84bbdeb8e684cd29039e828
Adhiraj Jain <adhirajjain2001@gmail.com>
2022-06-11 22:21:38 +1000

    update footer with more info

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import "../../Stylesheets/Footer.css";
+import "../../Stylesheets/Footer.scss";
 
 const Footer = () => {
     return (
-        <div id="footer" className='footer'>
+        <footer id="footer" className=''>
             <ul className="">
                 <li>
                     <a href="https://www.linkedin.com/in/adhiraj-jain/"><i className="fab fa-linkedin-in"></i></a>
@@ -15,7 +15,10 @@ const Footer = () => {
                     <a href="https://twitter.com/adhirajjain09"><i className="fab fa-twitter"></i></a>
                 </li>
             </ul>
-        </div>
+            <span className="copyright-div">© 2022 by Adhiraj Jain</span>
+            <span className="copyright-div">All product names, images, logos, and brands are property of their respective owners</span>
+        </footer>
+
     );
 };
 
