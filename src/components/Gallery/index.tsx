d780a5e124bbe43d01052b69ec5f8068bc714d16
s3821245 <s3821245@student.rmit.edu.au>
2023-12-02 01:58:30 +1100

    Add toggle switch

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -5,6 +5,10 @@ import mydata from '../../resume.json';
 import styles from "./Gallery.module.scss";
 
 import InfiniteScroll from 'react-infinite-scroll-component';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 
 const UnsplashImage = ({ url, key }) => (
     <div className={styles.image_item} key={key} >
@@ -12,16 +16,29 @@ const UnsplashImage = ({ url, key }) => (
     </div>
 );
 
+
+const theme = createTheme({
+    palette: {
+        toggleBtnGroup: {
+            light: '#ff7961',
+            main: '#f44336',
+            dark: '#ba000d',
+            contrastText: '#fff',
+        },
+    },
+});
+
 interface FuncProps {
 }
 
 const Gallery: React.FC<FuncProps> = (props) => {
 
     var _ = require('lodash');
-    const [images, setImages] = useState<number[]>([10]);
+    const [images, setImages] = useState<number[]>([]);
     const [loaded, setIsLoaded] = useState(false);
     const imageData = mydata["imageData"];
     const [hasMore, setHasMore] = useState(true);
+    const [portfolioType, setportfolioType] = useState("FASHION");
 
     useEffect(() => {
         fetchImages();
@@ -29,7 +46,7 @@ const Gallery: React.FC<FuncProps> = (props) => {
 
     const fetchImages = (count = 10) => {
         // setImages([...images, ...res.data]);
-        if (images.length + count > imageData["FASHION"]) {
+        if (images.length + count > imageData[portfolioType]) {
             setHasMore(false);
             return;
         }
@@ -38,38 +55,60 @@ const Gallery: React.FC<FuncProps> = (props) => {
         console.log(images);
     };
 
+    const changePortfolioType = () => {
+        if (portfolioType.match("work")) {
+            setportfolioType("volunteer")
+        }
+        else {
+            setportfolioType("work")
+        }
+    }
+
+    const handleChange = (
+        event: React.MouseEvent<HTMLElement>,
+        newPortfolioType: string,
+    ) => {
+        setportfolioType(newPortfolioType);
+        // setImages(_.concat(images, _.range(images.length, images.length + count)));
+        // setIsLoaded(true);
+    };
+
     return (
-        <section id="gallery">
-            <div className="project-heading">
-                <span className="section-headings">Projects</span>
-            </div>
+        <section id="gallery" className={styles.gallery}>
             <div className="">
-                <div className="">
-                    <div className={styles.container}>
-                        <InfiniteScroll
-                            dataLength={images.length}
-                            next={() => fetchImages(5)}
-                            hasMore={hasMore}
-                            loader={
-                                <img
-                                    src="https://res.cloudinary.com/chuloo/image/upload/v1550093026/scotch-logo-gif_jq4tgr.gif"
-                                    alt="loading"
+                <span className={styles.section_headings}>Gallery</span>
+            </div>
+            <ThemeProvider theme={theme}>
+                <ToggleButtonGroup
+                    color="toggleBtnGroup"
+                    value={portfolioType}
+                    exclusive
+                    onChange={handleChange}
+                    aria-label="Platform"
+                    className={styles.toggleButtonGroup}
+                >
+                    <ToggleButton value="FASHION" className={styles.toggleButton}>FASHION</ToggleButton>
+                    <ToggleButton value="WEDDING_BRIDAL" className={styles.toggleButton}>WEDDING/BRIDAL</ToggleButton>
+                    <ToggleButton value="EVENTS" className={styles.toggleButton}>EVENTS</ToggleButton>
+                </ToggleButtonGroup>
+            </ThemeProvider>
+            <div className={styles.container}>
+                <InfiniteScroll
+                    dataLength={images.length}
+                    next={() => fetchImages(5)}
+                    hasMore={hasMore}
+                >
+                    <div className={styles.image_grid} style={{ marginTop: "30px" }}>
+                        {loaded
+                            ? images.map((image, index) => (
+                                <UnsplashImage
+                                    url={image}
+                                    key={index}
                                 />
-                            }
-                        >
-                            <div className={styles.image_grid} style={{ marginTop: "30px" }}>
-                                {loaded
-                                    ? images.map((image, index) => (
-                                        <UnsplashImage
-                                            url={image}
-                                            key={index}
-                                        />
-                                    ))
-                                    : ""}
-                            </div>
-                        </InfiniteScroll>
+                            ))
+                            : ""}
                     </div>
-                </div>
+                </InfiniteScroll>
             </div>
         </section>
 
