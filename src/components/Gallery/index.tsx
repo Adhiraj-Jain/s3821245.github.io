bc44af60eb8410d0e3b4595478c4f1843c6bf481
s3821245 <s3821245@student.rmit.edu.au>
2023-12-02 14:37:49 +1100

    Fixed errors and styles

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -2,20 +2,50 @@ import { useEffect, useState } from "react";
 import React from "react";
 import mydata from '../../resume.json';
 
-import styles from "./Gallery.module.scss";
 
 import InfiniteScroll from 'react-infinite-scroll-component';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { PaletteOptions, PaletteColorOptions, PaletteColor } from '@mui/material/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import LinearProgress from '@mui/material/LinearProgress';
+import Box from '@mui/material/Box';
+
+import styles from "./Gallery.module.scss";
+
+import _ from 'lodash';
 
 
-const UnsplashImage = ({ portfolioType, url, key }) => (
+const useStyles = makeStyles({
+    toggleButtonGroup: {
+        '& .MuiToggleButtonGroup-grouped': {
+            color: '#fff', // Replace with your color
+        },
+        '& .MuiToggleButtonGroup-grouped.Mui-selected': {
+            // backgroundColor: 'none', // Replace with your color
+            color: '#f44336'
+        },
+    },
+});
+
+declare module '@mui/material/styles' {
+    interface Palette {
+        toggleBtnGroup: PaletteColor;
+    }
+    interface PaletteOptions {
+        toggleBtnGroup?: PaletteColorOptions;
+    }
+}
+
+const UnsplashImage = ({ url, key, openModal }) => (
     <div className={styles.image_item} key={key} >
-        <img src={portfolioType + "/" + url + ".jpg"} />
+        <img
+            src={url}
+            onClick={() => openModal(url)} />
+        <div className={styles.view_image}>View Image</div>
     </div>
 );
-
 
 const theme = createTheme({
     palette: {
@@ -33,16 +63,34 @@ interface FuncProps {
 
 const Gallery: React.FC<FuncProps> = (props) => {
 
-    var _ = require('lodash');
     const [images, setImages] = useState<number[]>([]);
     const [loaded, setIsLoaded] = useState(false);
     const imageData = mydata["imageData"];
     const [hasMore, setHasMore] = useState(true);
     const [portfolioType, setportfolioType] = useState("FASHION");
+    const [modalSrc, setModalSrc] = useState<string | null>(null);
+    const classes = useStyles();
 
     useEffect(() => {
         fetchImages();
+        const handleEsc = (event) => {
+            if (event.keyCode === 27) closeModal();
+        };
+        window.addEventListener('keydown', handleEsc);
+
+        // Clean up event listener on component unmount
+        return () => {
+            window.removeEventListener('keydown', handleEsc);
+        };
     }, [portfolioType]);
+
+    const openModal = (src: string) => {
+        setModalSrc(src);
+    };
+
+    const closeModal = () => {
+        setModalSrc(null);
+    };
 
     const fetchImages = (count = 10) => {
         if (imageData[portfolioType] <= 0) {
@@ -53,27 +101,22 @@ const Gallery: React.FC<FuncProps> = (props) => {
 
         if (images.length >= imageData[portfolioType] || images.length + count >= imageData[portfolioType]) {
             setImages(_.concat(_.range(0, imageData[portfolioType] + 1)));
-            // setHasMore(false);
+            setHasMore(false);
         } else {
             setImages(_.concat(images, _.range(images.length, images.length + count)));
+            setHasMore(true);
         }
         setIsLoaded(true);
         console.log(images);
     };
 
-    const changePortfolioType = () => {
-        if (portfolioType.match("work")) {
-            setportfolioType("volunteer")
-        }
-        else {
-            setportfolioType("work")
-        }
-    }
-
     const handleChange = (
         event: React.MouseEvent<HTMLElement>,
         newPortfolioType: string,
     ) => {
+        if (newPortfolioType === null || newPortfolioType === portfolioType) {
+            return;
+        }
         setportfolioType(newPortfolioType);
     };
 
@@ -84,12 +127,12 @@ const Gallery: React.FC<FuncProps> = (props) => {
             </div>
             <ThemeProvider theme={theme}>
                 <ToggleButtonGroup
-                    color="toggleBtnGroup"
+                    // color="toggleBtnGroup"
                     value={portfolioType}
                     exclusive
                     onChange={handleChange}
                     aria-label="Platform"
-                    className={styles.toggleButtonGroup}
+                    className={styles.toggleButtonGroup + " " + classes.toggleButtonGroup}
                 >
                     <ToggleButton value="FASHION" className={styles.toggleButton}>FASHION</ToggleButton>
                     <ToggleButton value="WEDDING_BRIDAL" className={styles.toggleButton}>WEDDING / BRIDAL</ToggleButton>
@@ -101,13 +144,16 @@ const Gallery: React.FC<FuncProps> = (props) => {
                     dataLength={images.length}
                     next={() => fetchImages(10)}
                     hasMore={hasMore}
+                    loader={<Box sx={{ width: '100%' }}>
+                        <LinearProgress />
+                    </Box>}
                 >
                     {imageData[portfolioType] > 0 && <div className={styles.image_grid} style={{ marginTop: "30px" }}>
                         {loaded
                             ? images.map((image, index) => (
                                 <UnsplashImage
-                                    portfolioType={portfolioType}
-                                    url={image}
+                                    url={portfolioType + "/" + image + ".jpg"}
+                                    openModal={openModal}
                                     key={index}
                                 />
                             ))
@@ -115,6 +161,21 @@ const Gallery: React.FC<FuncProps> = (props) => {
                     </div>}
                 </InfiniteScroll>
             </div>
+            {modalSrc && (
+                <div
+                    onClick={closeModal}
+                    style={{
+                        background: `RGBA(0,0,0,.5) url(${modalSrc}) no-repeat center`,
+                        backgroundSize: 'contain',
+                        width: '100%',
+                        height: '100%',
+                        position: 'fixed',
+                        zIndex: 10000,
+                        top: 0,
+                        left: 0,
+                    }}
+                />
+            )}
         </section>
 
 
