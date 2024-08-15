f6553be6af9cf359afa14acb1e837e52eaf29fa3
Adhiraj Jain <adhirajjain2001@gmail.com>
2024-08-15 21:58:35 +1000

    Enhanced UI for Experience ; Updated Data

--- src/components/Experience/EachWork.js
+++ src/components/Experience/EachWork.js
@@ -2,23 +2,34 @@ import React from 'react';
 
 const EachWork = (props) => {
     return (
-        <div className="row each-work my-4">
-            <div className="col-md-4">
-                <span className="d-block company">{props.work.company}</span>
-                <span className="d-block year">{props.work.year}</span>
-            </div>
-            <div className="col-md-8">
-                <span className="d-block position">{props.work.position}</span>
-                {props.buttonAvailable ? (<span key={0} className="col-md-8">
+     <li>
+        <div class="timeline-content">
+          <h3 class="date">{props.work.year}</h3>
+          <h1>{props.work.position}</h1>
+          <span className="d-block location"><i className="fas fa-map-marker"></i> {props.work.location}</span>
+          
+          {props.buttonAvailable ? (<span key={0} className="col-md-8">
                     {props.show !== props.value && (<button className="detail-button" value={props.value} onClick={props.onClicked} >Details</button>)}
-                    {props.show === props.value && (<span className="d-block summary"><span className="summary-heading">Summary: </span>{props.work.summary}</span>)}
-                </span>) : (<span key={1} className="col-md-8">
-                    <span className="d-block summary"><span className="summary-heading">Summary: </span>{props.work.summary}</span>
+                    {props.show === props.value && (<span className="d-block summary">{props.work.summary}</span>)}
+                </span>) : (<span key={1} className="">
+                    <span className="d-block summary">{props.work.summary}</span>
                 </span>)}
-                <span className="d-block location"><i className="fas fa-map-marker"></i> {props.work.location}</span>
-            </div>
-        </div >
+        </div>
+      </li>
     );
 };
 
 export default EachWork;
+
+
+//    <div className="row each-work my-4">
+//             <div className="col-md-4">
+//                 <span className="d-block company"></span>
+//                 <span className="d-block year">{props.work.year}</span>
+//             </div>
+//             <div className="col-md-8">
+//                 <span className="d-block position"></span>
+                
+//                 <span className="d-block location"><i className="fas fa-map-marker"></i> {props.work.location}</span>
+//             </div>
+//         </div >
--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -34,11 +34,24 @@ const Experience = () => {
     window.onresize = changeButton;
 
     return (
-        <section id="experience" className="experience-section">
-            <span className="section-headings">Experience</span>
-            <div className="experience-container container-fluid">
-                {Object.keys(mydata[workType]).map((key, index) => {
-                    return (<EachWork buttonAvailable={showButton} show={currWork} value={key} work={mydata[workType][key]} onClicked={showContent} key={index + workType.length} />)
+        <section id="experience" className="experience-section container">
+            <span className="section-headings d-inline">Experience</span>
+            <div className="experience-container">
+                {Object.keys(mydata[workType]).map((expKey, expKeyindex) => {
+                    const name = mydata[workType][expKey]["company"];
+                    const positions = mydata[workType][expKey]["positions"];
+                    return (<div className="row each-work my-4">
+                        <div className="company">{name}</div>
+                        <div className="timeline">
+                            <ul>
+                                {Object.keys(positions).map((key, index) => {
+                                    return (<EachWork buttonAvailable={showButton} show={currWork} value={key} work={positions[key]} onClicked={showContent} key={index + positions.length} />)
+                                }
+                                )}
+                            </ul>
+                        </div>
+                    </div>
+                    )
                 }
                 )}
             </div>
@@ -48,8 +61,28 @@ const Experience = () => {
                     <label className="switch-button-label"><span className="switch-button-label-span">Work</span></label>
                 </div>
             </div>
-        </section >
+        </section>
     );
 };
 
 export default Experience;
+
+// return (<EachWork buttonAvailable={showButton} show={currWork} value={key} work={positions[key]} onClicked={showContent} key={index + positions.length} />)
+
+
+// <section id="experience" className="experience-section">
+// <span className="section-headings">Experience</span>
+// <div className="experience-container container-fluid">
+//     {Object.keys(mydata[workType]).map((key, index) => {
+//         return (<EachWork buttonAvailable={showButton} show={currWork} value={key} work={mydata[workType][key]} onClicked={showContent} key={index + workType.length} />)
+//     }
+//     )}
+// </div>
+// <div className='switch'>
+//     <div className="switch-button">
+//         <input className="switch-button-checkbox" type="checkbox" onClick={changeWorkType}></input>
+//         <label className="switch-button-label"><span className="switch-button-label-span">Work</span></label>
+//     </div>
+// </div>
+// </section >
+
